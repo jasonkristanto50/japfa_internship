bb51683f61ec563a0aa706b5f18b28ccb9e11b6e
jasonkristanto50 <87260014+jasonkristanto50@users.noreply.github.com>
2025-05-22 21:51:44 +0700

    fix

--- lib/admin_page/pendaftaran_magang_detail_page.dart
+++ lib/admin_page/pendaftaran_magang_detail_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:http/retry.dart';
 import 'package:japfa_internship/authentication/login.dart';
 import 'package:japfa_internship/authentication/login_provider.dart';
 import 'package:japfa_internship/components/widget_component.dart';

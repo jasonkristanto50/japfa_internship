16b9769d49b14909d915d99731dc3649740c2e97
jasonkristanto50 <87260014+jasonkristanto50@users.noreply.github.com>
2025-06-09 22:12:39 +0700

    add port listen 8080

--- jpf_internship-api/server.js
+++ jpf_internship-api/server.js
@@ -13,7 +13,7 @@ require('dotenv').config(); // For environment variables
 const fuzzyLogic = require('./fuzzy_logic/fuzzy_logic');
 
 const app = express();  
-const port = process.env.PORT || 3000;  
+const port = process.env.PORT || 3000 || 8080;  
 
 // Middleware  
 app.use(cors());  

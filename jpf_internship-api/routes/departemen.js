ba979de0cfc58aa95cf89990f7a54f67834bf564
jasonkristanto50 <87260014+jasonkristanto50@users.noreply.github.com>
2025-06-11 16:05:12 +0700

    add more error checking

--- jpf_internship-api/routes/departemen.js
+++ jpf_internship-api/routes/departemen.js
@@ -96,7 +96,7 @@ router.get('/fetch-all-departemen', async (req, res) => {
         const result = await pool.query('SELECT * FROM DEPARTEMEN ORDER BY id_departemen ASC');
         res.status(200).json(result.rows);
     } catch (error) {
-        console.error('Error fetching Departemen:', error);
+        console.error('Error fetching Departemen:', error.message, error.stack);
         res.status(500).json({ error: 'Server error' });
     }
 });

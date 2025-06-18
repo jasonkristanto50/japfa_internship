417d5a4f43d1539a0c48b17b6933b3824a87f9ce
jasonkristanto50 <87260014+jasonkristanto50@users.noreply.github.com>
2025-06-18 23:26:15 +0700

    fix data univ & jurusan

--- lib/data.dart
+++ lib/data.dart
@@ -30,6 +30,11 @@ final List<Map<String, String>> universities = [
     'akreditasi': 'A'
   },
   {
+    'value': 'Universitas Negeri Jember',
+    'name': 'Universitas Negeri Jember',
+    'akreditasi': 'A'
+  },
+  {
     'value': 'Universitas Padjadjaran',
     'name': 'Universitas Padjadjaran',
     'akreditasi': 'B'
@@ -74,6 +79,192 @@ final List<Map<String, String>> universities = [
     'name': 'Universitas Kristen Satya Wacana',
     'akreditasi': 'C'
   },
+  // New universities added
+  {
+    'value': 'Universitas Terbuka',
+    'name': 'Universitas Terbuka',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Negeri Jakarta',
+    'name': 'Universitas Negeri Jakarta',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Jenderal Soedirman',
+    'name': 'Universitas Jenderal Soedirman',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Muhammadiyah Yogyakarta',
+    'name': 'Universitas Muhammadiyah Yogyakarta',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Islam Indonesia',
+    'name': 'Universitas Islam Indonesia',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Sanata Dharma',
+    'name': 'Universitas Sanata Dharma',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Kristen Immanuel',
+    'name': 'Universitas Kristen Immanuel',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Surya',
+    'name': 'Universitas Surya',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Esa Unggul',
+    'name': 'Universitas Esa Unggul',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Gunadarma',
+    'name': 'Universitas Gunadarma',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Katolik Soegijapranata',
+    'name': 'Universitas Katolik Soegijapranata',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Tarumanagara',
+    'name': 'Universitas Tarumanagara',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Trisakti',
+    'name': 'Universitas Trisakti',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Mercu Buana',
+    'name': 'Universitas Mercu Buana',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Pancasila',
+    'name': 'Universitas Pancasila',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Diponegoro',
+    'name': 'Universitas Diponegoro',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Kristen Satya Wacana',
+    'name': 'Universitas Kristen Satya Wacana',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Bhayangkara Jakarta Raya',
+    'name': 'Universitas Bhayangkara Jakarta Raya',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Islam Negeri Syarif Hidayatullah',
+    'name': 'Universitas Islam Negeri Syarif Hidayatullah',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Andalas',
+    'name': 'Universitas Andalas',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Negeri Malang',
+    'name': 'Universitas Negeri Malang',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Kristen Duta Wacana',
+    'name': 'Universitas Kristen Duta Wacana',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Fajar',
+    'name': 'Universitas Fajar',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Siliwangi',
+    'name': 'Universitas Siliwangi',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Suryakancana',
+    'name': 'Universitas Suryakancana',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Bhakti Indonesia',
+    'name': 'Universitas Bhakti Indonesia',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas PGRI Adi Buana',
+    'name': 'Universitas PGRI Adi Buana',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Muhammadiyah Sumatera Utara',
+    'name': 'Universitas Muhammadiyah Sumatera Utara',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Kader Bangsa',
+    'name': 'Universitas Kader Bangsa',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Al-Azhar Indonesia',
+    'name': 'Universitas Al-Azhar Indonesia',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Amikom Yogyakarta',
+    'name': 'Universitas Amikom Yogyakarta',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Jambi',
+    'name': 'Universitas Jambi',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Yudharta Pasuruan',
+    'name': 'Universitas Yudharta Pasuruan',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Pahlawan Tuanku Tambusai',
+    'name': 'Universitas Pahlawan Tuanku Tambusai',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Borobudur',
+    'name': 'Universitas Borobudur',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Muhammadiyah Sidoarjo',
+    'name': 'Universitas Muhammadiyah Sidoarjo',
+    'akreditasi': 'B'
+  },
+  {
+    'value': 'Universitas Tama Jagakarsa',
+    'name': 'Universitas Tama Jagakarsa',
+    'akreditasi': 'B'
+  }
 ];
 
 final List<Map<String, String>> majors = [
@@ -86,9 +277,50 @@ final List<Map<String, String>> majors = [
   {'value': 'Teknik Sipil', 'name': 'Teknik Sipil'},
   {'value': 'Pendidikan', 'name': 'Pendidikan'},
   {'value': 'Psikologi', 'name': 'Psikologi'},
-  {'value': 'Kedokteran', 'name': 'Kedokteran'}
+  {'value': 'Kedokteran', 'name': 'Kedokteran'},
+
+  // New majors added
+  {'value': 'Teknik Elektro', 'name': 'Teknik Elektro'},
+  {'value': 'Teknik Mesin', 'name': 'Teknik Mesin'},
+  {'value': 'Ilmu Hukum', 'name': 'Ilmu Hukum'},
+  {'value': 'Desain Komunikasi Visual', 'name': 'Desain Komunikasi Visual'},
+  {'value': 'Ilmu Ekonomi', 'name': 'Ilmu Ekonomi'},
+  {'value': 'Keperawatan', 'name': 'Keperawatan'},
+  {'value': 'Ilmu Pertanian', 'name': 'Ilmu Pertanian'},
+  {'value': 'Teknik Pertambangan', 'name': 'Teknik Pertambangan'},
+  {'value': 'Arsitektur', 'name': 'Arsitektur'},
+  {'value': 'Seni Musik', 'name': 'Seni Musik'},
+
+  {'value': 'Seni Rupa', 'name': 'Seni Rupa'},
+  {'value': 'Ilmu Sosiologi', 'name': 'Ilmu Sosiologi'},
+  {'value': 'Peternakan', 'name': 'Peternakan'},
+  {'value': 'Kesehatan Masyarakat', 'name': 'Kesehatan Masyarakat'},
+  {'value': 'Teknologi Pangan', 'name': 'Teknologi Pangan'},
+  {'value': 'Biologi', 'name': 'Biologi'},
+  {'value': 'Kimia', 'name': 'Kimia'},
+  {'value': 'Fisika', 'name': 'Fisika'},
+  {'value': 'Analisis Keuangan', 'name': 'Analisis Keuangan'},
+  {'value': 'Manajemen Pemasaran', 'name': 'Manajemen Pemasaran'},
+
+  {
+    'value': 'Manajemen Sumber Daya Manusia',
+    'name': 'Manajemen Sumber Daya Manusia'
+  },
+  {'value': 'Pendidikan Bahasa Inggris', 'name': 'Pendidikan Bahasa Inggris'},
+  {'value': 'Pendidikan Agama', 'name': 'Pendidikan Agama'},
+  {
+    'value': 'Bahasa dan Sastra Indonesia',
+    'name': 'Bahasa dan Sastra Indonesia'
+  },
+  {'value': 'Ilmu Lingkungan', 'name': 'Ilmu Lingkungan'},
+  {'value': 'Administrasi Publik', 'name': 'Administrasi Publik'},
+  {'value': 'Manajemen Informatika', 'name': 'Manajemen Informatika'},
+  {'value': 'Pengembangan Bisnis', 'name': 'Pengembangan Bisnis'},
+  {'value': 'Ilmu Data', 'name': 'Ilmu Data'},
+  {'value': 'Teknologi Informasi', 'name': 'Teknologi Informasi'}
 ];
 
+// Tidak digunakan lagi
 final List<Map<String, dynamic>> pembimbingPesertaData = [
   {
     'nama_pembimbing': 'Agus Bambang',
